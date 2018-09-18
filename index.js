883ca280a2d46c787721a785ce54b491d4f3758f
Jan Spencer Catalan <jancatalan2@gmail.com>
2018-09-18 03:11:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 function sayHiToGrandma(string) {
-  if(string == string.)
+  if(string == string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+  
 }
