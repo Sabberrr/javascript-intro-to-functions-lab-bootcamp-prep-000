dd8e9a3cfae6060a8910b387f3467b405138aabb
Jan Spencer Catalan <jancatalan2@gmail.com>
2018-09-18 03:14:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ function shout (string) {
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
 function sayHiToGrandma(string) {
   if(string == string.toLowerCase()) {
     return "I can't hear you!"
   }
-  
+}
+function sayHiToGrandma(string) {
+  if(string == string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+}
+function sayHitoGrandma(string) {
+  if (string == "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
